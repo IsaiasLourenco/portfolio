36525ea63d0579739bfec8a10d6005c2528216c3
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2023-03-12 02:34:13 -0300

    Acerto layout do contatoo

--- within_quote.php
+++ within_quote.php
@@ -30,29 +30,41 @@
             objetivo de ver nossos clientes satisfeitos e realizados com suas
             empresas informatizadas.
           </p>
+          <h5 class="mb-2">
+            Entre em contato
+          </h5>
+
+
           <div class="d-flex align-items-center mt-2 wow zoomIn" data-wow-delay="0.6s">
-            <div class="bg-primary d-flex align-items-center justify-content-center rounded"
-              style="width: 60px; height: 60px">
-              <a class="bi bi-telephone-inbound-fill text-white" href=""></a>
+            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px">
+              <a class="bi bi-telephone-inbound-fill text-white" href="phone.php"></a>
             </div>
-            <div class="ps-4">
-              <h5 class="mb-2">Entre em contato</h5>
-              <h4 class="text-primary mb-0">(19) 19 99674 5466</h4>
-            </div>
+            <h4 class="text-primary mb-0">&nbsp;&nbsp;(19)99674-5466</h4>
           </div>
+
+          <div class="d-flex align-items-center mt-2 wow zoomIn" data-wow-delay="0.6s">
+
+            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px">
+              <a class="bi bi-whatsapp text-white" href="zap.php"></a>
+            </div>
+            <h4 class="text-primary mb-0">&nbsp;&nbsp;(19)99674-5466</h4>
+          </div>
+
         </div>
+
         <div class="col-lg-5">
-          <div class="bg-primary rounded h-100 d-flex align-items-center p-5 wow zoomIn" data-wow-delay="0.9s">
+          <h4 class="fw-bold text-primary">
+            Mande-nos um email
+          </h4>
+          <div class=" bg-primary rounded h-100 d-flex align-items-center p-5 wow zoomIn" data-wow-delay="0.9s">
 
             <form method="post" action="orcamentos.php">
               <div class="row g-3">
                 <div class="col-xl-12">
-                  <input type="text" class="form-control bg-light border-0" name="nome_msg" placeholder="Seu nome"
-                    style="height: 55px" required />
+                  <input type="text" class="form-control bg-light border-0" name="nome_msg" placeholder="Seu nome" style="height: 55px" required />
                 </div>
                 <div class="col-12">
-                  <input type="email" class="form-control bg-light border-0" name="email_msg" placeholder="Seu Email"
-                    style="height: 55px" required />
+                  <input type="email" class="form-control bg-light border-0" name="email_msg" placeholder="Seu Email" style="height: 55px" required />
                 </div>
                 <div class="col-12">
                   <select name="plano_msg" class="form-select bg-light border-0" required style="height: 55px">
@@ -63,8 +75,7 @@
                   </select>
                 </div>
                 <div class="col-12">
-                  <textarea class="form-control bg-light border-0" rows="3" name="mensagem_msg" placeholder="Mensagem"
-                    required></textarea>
+                  <textarea class="form-control bg-light border-0" rows="3" name="mensagem_msg" placeholder="Mensagem" required></textarea>
                 </div>
                 <div class="col-12">
                   <button class="btn btn-dark w-100 py-3" type="submit">
